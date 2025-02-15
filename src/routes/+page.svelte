2d6d334b4bbb8a007498877cf22b68fe1eb3cd80
Aditya Oberai <adityaoberai1@gmail.com>
2025-02-15 12:25:53 +0530

    Add meta tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,11 @@
     export let data; // Accepts props for blog data
 </script>
 
+<svelte:head>
+<title>Blog Demo<title>
+<meta name="description" content="A test blog site built with SvelteKit and Markdoc">
+</svelte:head>
+
 <div class="container">
     <h1>Aditya Oberai's Blog</h1>
     <ul>
